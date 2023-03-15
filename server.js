46e23e7bd364322102e287abe121a725003423c6
880404 <127268099+880404@users.noreply.github.com>
2023-03-15 10:28:54 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const username = process.env.WU || "admin";
 const password = process.env.WPD || "password";
-const url = process.env.RENDER_EXTERNAL_URL;
+const url = process.env.SERVER_URL;
 const port = process.env.PORT || 3000;
 const express = require("express");
 const app = express();
